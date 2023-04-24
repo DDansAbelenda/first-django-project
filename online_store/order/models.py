fcb694a58e1989f890ceeb74406de8eb16a8e16d
Daniel Dans Abelenda <ddansabelenda@gmail.com>
2023-04-24 11:22:29 -0600

    adding classes to models.py

--- online_store/order/models.py
+++ online_store/order/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(8)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    section = models.CharField(max_length=30)
+    price = models.FloatField()
+
+
+class Order(models.Model):
+    number = models.IntegerField()
+    date = models.DateField()
+    is_delivered = models.BooleanField()
