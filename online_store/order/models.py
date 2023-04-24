f186dea89d20924ae27379bebc89b4588bbda715
Daniel Dans Abelenda <ddansabelenda@gmail.com>
2023-04-24 13:20:48 -0600

    adding tables to the database and changing the size of the name field

--- online_store/order/models.py
+++ online_store/order/models.py
@@ -1,22 +1,23 @@
-from django.db import models
+from django.db.models import *
 
 
 # Create your models here.
 
-class Client(models.Model):
-    name = models.CharField(max_length=30)
-    address = models.CharField(max_length=50)
-    email = models.EmailField()
-    phone = models.CharField(max_length=8)
+class Client(Model):
+    name = CharField(max_length=40)
+    address = CharField(max_length=50)
+    email = EmailField()
+    phone = CharField(max_length=8)
 
 
-class Item(models.Model):
-    name = models.CharField(max_length=30)
-    section = models.CharField(max_length=30)
-    price = models.FloatField()
+class Item(Model):
+    name = CharField(max_length=30)
+    section = CharField(max_length=30)
+    price = FloatField()
 
 
-class Order(models.Model):
-    number = models.IntegerField()
-    date = models.DateField()
-    is_delivered = models.BooleanField()
+
+class Order(Model):
+    number = IntegerField()
+    date = DateField()
+    is_delivered = BooleanField()
